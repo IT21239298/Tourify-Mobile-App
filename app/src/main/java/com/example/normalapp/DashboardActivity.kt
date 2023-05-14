7825f8683c952e8e337fea0df0ae9bda9a475032
Sathsara Sithum <it21239298@my.sliit.lk>
2023-05-14 09:33:34 +0530

    update driver

--- app/src/main/java/com/example/normalapp/DashboardActivity.kt
+++ app/src/main/java/com/example/normalapp/DashboardActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.view.ViewGroup
 import android.view.Window
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
@@ -26,6 +27,8 @@ class DashboardActivity : AppCompatActivity() {
     private lateinit var driverBirth: TextView
     private lateinit var driverGender: TextView
     private lateinit var driverAddres: TextView
+
+
     private lateinit var  btneditD : Button
     private lateinit var database: DatabaseReference
 // ...
@@ -42,9 +45,25 @@ class DashboardActivity : AppCompatActivity() {
         driverBirth = findViewById(R.id.DriverProfileBithday)
         driverGender = findViewById(R.id.DriverPorfileGender)
         driverAddres = findViewById(R.id.driverProfileAddress)
-       btneditD =  findViewById(R.id.dEditbtn)
+        btneditD =  findViewById(R.id.dEditbtn)
+
+
+
         btnLogOut  = findViewById(R.id.Logoutbtn)
 
+        btneditD.setOnClickListener{
+
+            val bottomSheet = BottomSheetDialog(this)
+            bottomSheet.requestWindowFeature(Window.FEATURE_NO_TITLE)
+            bottomSheet.setContentView(R.layout.edit_profile)
+            bottomSheet.window!!.setLayout(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+            bottomSheet.window!!.setBackgroundDrawable(ColorDrawable(android.graphics.Color.TRANSPARENT))
+
+
+        }
 
 
         btneditD.setOnClickListener{
@@ -61,10 +80,47 @@ class DashboardActivity : AppCompatActivity() {
             val  btnCancel: Button? = bottomSheet.findViewById(R.id.btneditcancel)
             val btnEdited: Button ? = bottomSheet.findViewById(R.id.buttnedit)
 
+            val etdEmail:EditText? = bottomSheet.findViewById(R.id.drivereditEmail)
+            val etdName:EditText? = bottomSheet.findViewById(R.id.drivereditname)
+            val etdBirth:EditText? =bottomSheet.findViewById(R.id.drivereditbith)
+            val etdGender:EditText? =bottomSheet.findViewById(R.id.drivereditgender)
+            val etdAddres:EditText? =bottomSheet.findViewById(R.id.drivereditaddress)
+
+            val sEmail = driverEmail.text.toString()
+            val sName = driverName.text.toString()
+            val sAddress = driverAddres.text.toString()
+            val sBirth = driverBirth.text.toString()
+            val sGender = driverGender.text.toString()
+
+            etdEmail!!.setText(sEmail)
+            etdName!!.setText(sName)
+            etdAddres!!.setText(sAddress)
+            etdBirth!!.setText(sBirth)
+            etdGender!!.setText(sGender)
+
             btnCancel!!.setOnClickListener{
                 bottomSheet.dismiss()
             }
             btnEdited!!.setOnClickListener{
+
+                val dEmail =  etdEmail!!.text.toString()
+                val dName = etdName!!.text.toString()
+                val dBirth = etdBirth!!.text.toString()
+                val dGender =etdGender!!.text.toString()
+                val dAddres =etdAddres!!.text.toString()
+
+                val editMap = mapOf(
+
+                    "email" to dEmail,
+                    "name" to dName,
+                    "address" to dAddres,
+                    "birth" to dBirth,
+                    "gender" to dGender
+                )
+
+                val userDriverID = FirebaseAuth.getInstance().currentUser!!.uid
+
+                database.child("UserDriver").child(userDriverID).updateChildren(editMap)
                 Toast.makeText(this,"Edited successfully" ,Toast.LENGTH_SHORT).show()
                 bottomSheet.dismiss()
             }
@@ -107,6 +163,26 @@ class DashboardActivity : AppCompatActivity() {
         val secondActButton = findViewById<ImageButton>(R.id.driverpaymentbtn)
         secondActButton.setOnClickListener{
             val Intent = Intent(this,DriverPaymentActivity::class.java)
+            startActivity(Intent)
+        }
+
+        val thirdActButton = findViewById<ImageButton>(R.id.driverbankbtn)
+        thirdActButton.setOnClickListener{
+            val Intent = Intent(this,DriverAccountActivity::class.java)
+            startActivity(Intent)
+        }
+
+
+        val forthActButton = findViewById<ImageButton>(R.id. drivervehiclebtn)
+        forthActButton.setOnClickListener{
+            val Intent = Intent(this,DriverVehicleActivity::class.java)
+            startActivity(Intent)
+        }
+
+
+        val fifthActButton = findViewById<ImageButton>(R.id.   drivernotificationbtn)
+        fifthActButton.setOnClickListener{
+            val Intent = Intent(this,DriverNotificationActivity::class.java)
             startActivity(Intent)
         }
     }
